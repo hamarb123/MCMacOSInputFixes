eb91fd90ae53f479814056bc91d60c78c8d90252
decce6 <decce6@proton.me>
2025-09-14 07:08:00 +0800

    Fix incorrect method name

--- src/main/java/com/hamarb123/macos_input_fixes/compat/IxerisCompat.java
+++ src/main/java/com/hamarb123/macos_input_fixes/compat/IxerisCompat.java
@@ -4,7 +4,6 @@ import net.fabricmc.loader.api.FabricLoader;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 
 // Utility class for Ixeris mod compatibility (see issue #48)
 public class IxerisCompat {
@@ -17,10 +16,9 @@ public class IxerisCompat {
             // initialize Ixeris compatibility fields
             try {
                 Class<?> clazz = Class.forName("me.decce.ixeris.api.IxerisApi");
-                IXERIS_API = clazz.getDeclaredMethod("getInstance").invoke(null);
-                MethodType methodType = MethodType.methodType(void.class, Runnable.class);
+                IXERIS_API = clazz.getMethod("getInstance").invoke(null);
                 MethodHandles.Lookup lookup = MethodHandles.publicLookup();
-                RUN_ON_RENDER_THREAD = lookup.findVirtual(clazz, "runOnRenderThread", methodType);
+                RUN_ON_RENDER_THREAD = lookup.unreflect(clazz.getMethod("runLaterOnRenderThread", Runnable.class));
             } catch (Throwable t) {
                 throw new RuntimeException("Exception initializing Ixeris compat!", t);
             }
